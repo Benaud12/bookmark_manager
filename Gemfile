b775d12b3053cd6fb2de6ab226025e0a8b03875e
Michael Roger <bodgeuk12@gmail.com>
2015-09-23 11:48:14 +0100

    Adding ability to sign-in and remeber users details

--- Gemfile
+++ Gemfile
@@ -2,10 +2,11 @@ source "https://rubygems.org"
 
 ruby '2.2.1'
 
-gem 'sinatra'
+gem 'bcrypt'
 gem 'data_mapper'
 gem 'database_cleaner'
 gem 'dm-postgres-adapter'
+gem 'sinatra'
 
 group :test do
   gem 'capybara'
--- app/app.rb
+++ app/app.rb
@@ -3,10 +3,18 @@ require_relative '../data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
 
+  enable :sessions
+  set :session_secret, 'super secret'
+
   set :views, proc { File.join(root, '..', 'views') }
+
+  get '/' do
+    redirect ('/links')
+  end
 
   get '/links' do
     @links = Link.all
+    current_user
     erb :'links/index'
   end
 
@@ -28,6 +36,23 @@ class BookmarkManager < Sinatra::Base
     tag = Tag.first(name: params[:name])
     @links = tag ? tag.links : []
     erb :'links/index'
+  end
+
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email],
+                password: params[:password])
+    session[:user_id] = user.id
+    redirect to('/links')
+  end
+
+  helpers do
+    def current_user
+      @current_user = User.get(session[:user_id])
+    end
   end
 
 end
